e4fb1cbd63e5ca002ec37dc0c20ccee85ef1bbf1
kychan9044 <kychan9044@naver.com>
2024-05-28 20:21:27 +0900

    DataPortalSearcher 구현

--- app/py/autoupdater/DataPortalSearcher.py
+++ app/py/autoupdater/DataPortalSearcher.py
@@ -1,6 +1,13 @@
-from util.logger import logger
-from util.conf import config
+import sys
+from os import path
+sys.path.append(path.dirname( path.dirname( path.abspath(__file__) ) ))
+from autoupdater.util import logger
+from autoupdater.util.conf import config
 import abc
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlencode
+import traceback
 
 class IDataPortalSearcher:
     """
@@ -10,23 +17,84 @@ class IDataPortalSearcher:
         pass
 
     @abc.abstractmethod
-    def get_info(self, keyword:str):
+    def get_info_lists(self, keyword:str):
         pass
 
 class DataPortalSearcher(IDataPortalSearcher): 
     """
     공공데이터 포털 검색 구현체
     """
+    SEARCH_CONFIG = config['SEARCH_CONFIG']
+    search_params = {
+            'dType': 'FILE',
+            'keyword': '',
+            'operator': 'AND',
+            'recmSe': 'N',
+            'sort': 'updtDt',
+            'currentPage': '',
+            'perPage': SEARCH_CONFIG['SEARCH_PER_PAGE']
+        }
     def __init__(self) -> None:
         super().__init__()
         self.log = logger.get_logger(__name__)
         return
     
-    def get_info(self, keyword: str):
+    def get_info_lists(self, keyword: str):
         self.log.info(f"Start to search by keyword: {keyword}")
-        return
+        result = []
+
+        self.search_params['keyword'] = keyword
+        page = 1
+        try:
+            while True:
+                self.search_params['currentPage'] = str(page)
+                url = self.SEARCH_CONFIG['SEARCH_BASE_URL'] + urlencode(self.search_params)
+                ret = self._get_info_list(url)
+                if not ret:
+                    break
+                result.extend(ret)
+                page += 1
+        except Exception as e:
+            self.log.error(traceback.format_exc())
+            self.log.error(e)
+            self.log.error(f"Failed to get info list")
+        
+        return result
+    
+    def _get_info_list(self, url: str):
+        self.log.info(f"Start to get info list form {url}")
+        response = requests.get(url)
+        result= []
+
+        if response.status_code == config['WEB_STATUS']['OK']:
+            html = response.text
+            soup = BeautifulSoup(html, 'html.parser')
+            ul = soup.select_one(self.SEARCH_CONFIG['SEARCH_LIST_SELECTOR'])
+
+            titles = ul.select(self.SEARCH_CONFIG['TITLE_SELECTOR'])
+            providers = ul.select(self.SEARCH_CONFIG['PROVIDER_SELECTOR'])
+            dates = ul.select(self.SEARCH_CONFIG['MODIFIED_DATE_SELECTOR'])
+
+            if len(titles) == len(providers) == len(dates):
+                self.log.info("Succeed to get info list")
+                for title, provider, date in zip(titles, providers, dates):
+                    result.append({
+                        'title': title.get_text().strip(),
+                        'provider': provider.get_text().strip(),
+                        'date': date.get_text().strip()
+                    })
+                    self.log.info(result[-1]) 
+            else:
+                raise Exception("title, provider, date length is not equal")
+        else:
+            raise Exception(f"HTTP error: {response.status_code}")
+        
+        return result
+
     
 if __name__ == "__main__":
     search = DataPortalSearcher()
-    search.get_info(config['search_keyword'][0])
-    search.get_info(config['search_keyword'][1])
+    result = search.get_info_lists(config['SEARCH_CONFIG']['SEARCH_KEYWORD'][0])
+    print(len(result))
+    result = search.get_info_lists(config['SEARCH_CONFIG']['SEARCH_KEYWORD'][1])
+    print(len(result))
--- app/py/autoupdater/util/conf.py
+++ app/py/autoupdater/util/conf.py
@@ -1,6 +1,15 @@
 config = \
 {
-    'search_url' : 'https://www.data.go.kr/tcs/dss/selectDataSetList.do?dType=FILE&detailKeyword=&publicDataPk=&recmSe=N&detailText=&relatedKeyword=&commaNotInData=&commaAndData=&commaOrData=&must_not=&tabId=&dataSetCoreTf=&coreDataNm=&sort=_score&relRadio=&orgFullName=&orgFilter=&org=&orgSearch=&brm=&instt=&svcType=&kwrdArray=&extsn=&coreDataNmArray=&pblonsipScopeCode=&perPage=40',
-    'search_param' : {'keyword:':"keyword", 'keyword_condition':"operator", "page":"currentPage"},
-    'search_keyword': ['헌옷+수거함', '의류+수거함']
+    'SEARCH_CONFIG': {
+        'SEARCH_BASE_URL': 'https://www.data.go.kr/tcs/dss/selectDataSetList.do?',
+        'SEARCH_PER_PAGE': 40,
+        'SEARCH_LIST_SELECTOR': 'div.result-list > ul',
+        'TITLE_SELECTOR': 'span.title',
+        'PROVIDER_SELECTOR': 'div.info-data > p:nth-child(1) > span.data',
+        'MODIFIED_DATE_SELECTOR': 'div.info-data > p:nth-child(2) > span.data',
+        'SEARCH_KEYWORD': ['헌옷 수거함', '의류 수거함']
+    },
+    'WEB_STATUS':{
+        "OK": 200
+    }
 }
