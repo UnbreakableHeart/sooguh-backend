c1554aff88775611d2a528ee0663ae58621b0138
kychan9044 <kychan9044@naver.com>
2024-05-30 23:40:10 +0900

    DataPortalSearcher 모듈 Docstring 추가

--- app/py/autoupdater/DataPortalSearcher.py
+++ app/py/autoupdater/DataPortalSearcher.py
@@ -1,4 +1,11 @@
+"""Exploring the Public Data Portal and get the list of data by keyword
 
+This module is used to get the list of data by keyword.
+
+Example:
+    >>> searcher = DataPortalSearcher()
+    >>> result = searcher.get_info_lists('keyword')
+"""
 
 import sys
 from os import path
@@ -11,16 +18,39 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlencode
 import traceback
 from overrides import overrides
+from typing import List, Dict
 
 class IDataPortalSearcher:
+    """An abstract base class for data portal searchers.
+
+    This interface defines the methods that should be implemented by a class that search for data in a data portal.
+    """
     def __init__(self) -> None:
         pass
 
     @abc.abstractmethod
-    def get_info_lists(self, keyword:str):
+    def get_info_lists(self, keyword:str) -> List[Dict[str, str]]:
+        """Abstract method to get the list of data by a given keyword.
+        
+        Searches for information from the data portal and returns a list of dictionaries containing information about the data found by the given keyword. 
+        Each dictionary should have the following keys: 'title', 'provider', 'date', and 'link'.
+
+        Parameters:
+            keyword (str): The keyword to search for in the data portal.
+
+        Returns:
+            List[Dict[str, str]]: A list of dictionaries containing information about the data found by the given keyword. Each dictionary should have the following keys: 'title', 'provider', 'date', and 'link'.
+        """
         pass
 
 class DataPortalSearcher(IDataPortalSearcher): 
+    """A class for searching data in a data portal.
+
+    Attributes:
+        SEARCH_CONFIG (dict): Configuration parameters for the search.
+        search_params (dict): Parameters for the search query.            
+    """
+
     SEARCH_CONFIG = config['SEARCH_CONFIG']
     search_params = {
             'dType': 'FILE',
@@ -37,7 +67,22 @@ class DataPortalSearcher(IDataPortalSearcher):
         return
     
     @overrides
-    def get_info_lists(self, keyword: str):
+    def get_info_lists(self, keyword: str) -> List[Dict[str, str]]:
+        """ Search data portal using the given keyword and return the list of data.
+        
+        This method searches for all pages and all information from the data portal.
+        And it returns a list of dictionaries containing information about the data found by the given keyword.
+
+        Args:
+            keyword (str): The keyword to search for in the data portal.
+
+        Returns:
+            List[Dict[str, str]]: A list of dictionaries containing information about the data found by the given keyword. Each dictionary should have the following keys: 'title', 'provider', 'date', and 'link'.
+                title (str): The title of the data.
+                provider (str): The name of the organization that provided the information.
+                date (str): The date when the data was last updated.
+                link (str): The link to the data file.
+        """
         self.log.info(f"Start to search by keyword: {keyword}")
         result = []
 
@@ -59,7 +104,25 @@ class DataPortalSearcher(IDataPortalSearcher):
         
         return result
     
-    def _get_info_list(self, url: str):
+    def _get_info_list(self, url: str) -> List[Dict[str, str]]:
+        """Get search results from the given data portal page and return a list of dictionaries containing information.
+        
+        This method searches only from the given url.
+
+        Args:
+            url (str): The URL of the data portal page to scrape.
+
+        Returns:
+            List[Dict[str, str]]: A list of dictionaries containing information about the data found by the given keyword. Each dictionary should have the following keys: 'title', 'provider', 'date', and 'link'.
+                title (str): The title of the data.
+                provider (str): The name of the organization that provided the information.
+                date (str): The date when the data was last updated.
+                link (str): The link to the data file.
+
+        Exceptions:
+            Exception: If the response status code is not 200.
+            Exception: If the length of titles, providers, dates, and links are not equal.
+        """
         self.log.info(f"Start to get info list form {url}")
         response = requests.get(url)
         result= []
