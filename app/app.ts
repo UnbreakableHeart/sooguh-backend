474d95b9fa9ba94818780dbb05e753a88a3c1933
kychan9044 <kychan9044@naver.com>
2024-08-21 18:10:01 +0900

    route swagger

--- app/app.ts
+++ app/app.ts
@@ -3,6 +3,8 @@ import Logger from "./src/config/logger";
 import dotenv from "dotenv";
 import HomeRouter from "./src/routes/home";
 import DatabaseSingleton from "./src/config/db";
+import swaggerUI from 'swagger-ui-express';
+import swaggerJson from "./src/swagger/swagger-output.json";
 
 const logger = Logger.getInstance();
 class App{
@@ -23,11 +25,13 @@ class App{
     private initMiddleware(){
         this.application.use(express.json());
         this.application.use(express.urlencoded({extended: true}));
+        
     };
 
     private initRoutes(){
         const home = new HomeRouter().getRouter();
         this.application.use("/", home);
+        this.application.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJson));
     };
 
     public getApplication(): express.Application{
